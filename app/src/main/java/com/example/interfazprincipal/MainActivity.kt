488ab9e3e82ba0c1216024722f68758735b1dd22
lisbethceron1 <flceron0@misena.edu.co>
2024-03-01 11:23:40 -0500

    faltan puntos

--- app/src/main/java/com/example/interfazprincipal/MainActivity.kt
+++ app/src/main/java/com/example/interfazprincipal/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.interfazprincipal
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
@@ -12,35 +17,45 @@ class MainActivity : AppCompatActivity(), ViewPagerAdapter.OnItemSelected {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewPager = findViewById(R.id.viewPager)
+
+        viewPager = findViewById(R.id.viewPager);if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val controller = window.insetsController
+            controller?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
 
         boardList = listOf(
             Board(
                 fondo = R.color.AZUL,
                 imagen = R.drawable.uno,
-                titulo = "lorem ipsum",
-                descripcion = "holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                titulo = "Sobre Nosotros",
+                descripcion = "Somos una plataforma de aprendizaje que ofrece clases personalizadas impartidas por instructores expertos. Nuestra misión es proporcionar una experiencia educativa adaptada a las necesidades individuales de cada estudiante."
 
             ),
             Board(
-                fondo = R.color.black,
+                fondo = R.color.AZUL,
                 imagen = R.drawable.dos,
-                titulo = "lorem ipsum",
-                descripcion = "holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                titulo = "Clases Personalizadas",
+                descripcion = "Nuestras clases personalizadas permiten a los estudiantes aprender a su propio ritmo, recibir retroalimentación específica y utilizar recursos adaptados a sus necesidades, lo que aumenta su confianza y maximiza su potencial académico."
 
             ),
             Board(
-                fondo = R.color.black,
+                fondo = R.color.AZUL,
                 imagen = R.drawable.tres,
-                titulo = "lorem ipsum",
-                descripcion = "holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                titulo = "Modalidad Virtual o Presencial",
+                descripcion = "Ofrecemos opciones flexibles de clases virtuales y presenciales para adaptarnos al estilo de vida de cada estudiante. Las clases virtuales brindan conveniencia, mientras que las presenciales ofrecen una experiencia más interactiva"
 
             ),
             Board(
-                fondo = R.color.black,
+                fondo = R.color.AZUL,
                 imagen = R.drawable.cuatro,
-                titulo = "lorem ipsum",
-                descripcion = "holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                titulo = "Contraoferta",
+                descripcion = "En nuestra plataforma, los estudiantes tienen la libertad de publicar sus requisitos de clase y recibir múltiples ofertas de instructores. Esta modalidad única permite a los estudiantes comparar precios, ubicaciones y condiciones de enseñanza antes de comprometerse."
 
             )
 
