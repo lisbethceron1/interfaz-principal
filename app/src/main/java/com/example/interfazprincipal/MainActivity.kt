8042e1b0c0783943ae5941d833f96c657d729a3c
lisbethceron1 <flceron0@misena.edu.co>
2024-03-01 02:12:54 -0500

    main

--- app/src/main/java/com/example/interfazprincipal/MainActivity.kt
+++ app/src/main/java/com/example/interfazprincipal/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.interfazprincipal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var boardList: List<Board>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.board_item)
+        setContentView(R.layout.activity_main)
+
+        boardList = listOf(
+            Board(
+                fondo = R.color.black,
+                imagen = R.drawable.uno,
+                titulo = "lorem ipsummm",
+                descripcion = "holaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+            ),
+            Board(
+                fondo = R.color.black,
+                imagen = R.drawable.dos,
+                titulo = "lorem ipsummm",
+                descripcion = "holaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+            ),
+            Board(
+                fondo = R.color.black,
+                imagen = R.drawable.tres,
+                titulo = "lorem ipsummm",
+                descripcion = "holaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+            ),
+            Board(
+                fondo = R.color.black,
+                imagen = R.drawable.cuatro,
+                titulo = "lorem ipsummm",
+                descripcion = "holaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+            )
+
+        )
+        val adapter = ViewPagerAdapter(boardList)
+        viewPager.adapter = adapter
     }
 }
